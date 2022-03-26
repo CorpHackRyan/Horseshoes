f8ee2aa129bc197f4bfeaba979a2d2433c893247
root <ryanoconnor010@gmail.com>
2022-03-26 12:32:00 -0400

    initial commit of entire project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=0,maximum-scale=1">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/fatloss/style.css">
+    <title>Weight Loss Competition - January 9th - April 3rd, 2022</title>
+    <link rel="icon" type="image/x-icon" href="/fatloss/favicon.ico">
+    <meta property="og:title" content="2022 Weight loss competition" />
+    <meta property="og:description" content="January 9th - April 3rd Weight loss journey for all!" />
+    <meta property="og:image" content="http://www.nonemoreplump.com/scale.jpg" />
+
+</head>
+<body></body>
+
+
+
+</html>
